eea16c6821c6f12159f142c44a6ee6670bac4055
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2016-05-03 14:00:28 +0200

    fixed vimant.py for new vim

--- tools/vimant.py
+++ tools/vimant.py
@@ -4,10 +4,10 @@ import re, sys, subprocess, os
 
 TARGET_RE = re.compile('^([a-zA-Z0-9_-]+):$')
 ERRMSG_RE = re.compile('^    \\[javac\\] (.*\\.java):([0-9]+): ([^ ].*)$')
-ERRSYMBOL_RE = re.compile('^    \\[javac\\] symbol *: ([^ ].*)$')
-ERRLOCATION_RE = re.compile('^    \\[javac\\] location *: ([^ ].*)$')
-ERRFOUND_RE = re.compile('^    \\[javac\\] found *: ([^ ].*)$')
-ERRREQUIRED_RE = re.compile('^    \\[javac\\] required *: ([^ ].*)$')
+ERRSYMBOL_RE = re.compile('^    \\[javac\\] +symbol *: +([^ ].*)$')
+ERRLOCATION_RE = re.compile('^    \\[javac\\] +location *: +([^ ].*)$')
+ERRFOUND_RE = re.compile('^    \\[javac\\] +found *: +([^ ].*)$')
+ERRREQUIRED_RE = re.compile('^    \\[javac\\] +required *: +([^ ].*)$')
 ERRLINE_RE = re.compile('^    \\[javac\\] (.*)$')
 ERRCOL_RE = re.compile('^    \\[javac\\] ([ \\t]*)\\^$')
 
@@ -38,6 +38,7 @@ class OutputReader(object):
 			rm = ERRREQUIRED_RE.match(line)
 			lm = ERRLINE_RE.match(line)
 			cm = ERRCOL_RE.match(line)
+			em = ERRMSG_RE.match(line)
 			if sm:
 				if len(self.errextra):
 					self.errextra += ', '
@@ -54,12 +55,17 @@ class OutputReader(object):
 				if len(self.errextra):
 					self.errextra += ', '
 				self.errextra += 'required: ' + rm.group(1)
-			elif lm:
-				self.state = OutputReader.ST_ERRLINE
 			elif cm:
 				self.errcol = str(len(cm.group(1)) + 1)
+				#self.dumpError()
+				#self.state = OutputReader.ST_NONE
+			elif em:
 				self.dumpError()
 				self.state = OutputReader.ST_NONE
+				self.procLine(line)
+			elif lm:
+				#self.state = OutputReader.ST_ERRLINE
+				pass
 			else:
 				self.dumpError()
 				self.state = OutputReader.ST_NONE
@@ -82,6 +88,9 @@ class OutputReader(object):
 		if len(self.errextra):
 			msg += ' (' + self.errextra + ')'
 		print(msg)
+	def endOutput(self):
+		if self.state != OutputReader.ST_NONE:
+			self.dumpError()
 
 proc = subprocess.Popen(['ant'] + sys.argv[1:], stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
 reader = OutputReader()
@@ -91,5 +100,6 @@ while True:
 		break
 	line = line.rstrip('\r\n')
 	reader.procLine(line)
+reader.endOutput()
 status = proc.wait()
 sys.exit(status)
