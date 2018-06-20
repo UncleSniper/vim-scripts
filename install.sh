c4e99f70661bae3adcd6d04da96ef7b7205b30bd
Simon Bausch <simon.bausch@cellent.de>
2018-06-20 15:58:10 +0200

    fixed misuse of test(1) in install.sh

--- install.sh
+++ install.sh
@@ -5,17 +5,17 @@ here="$(dirname -- "$(readlink -f -- "$0")")"
 mkdir -p ~/.vim/{autoload,bundle,syntax}
 [ -f ~/.vim/autoload/pathogen.vim ] || wget https://github.com/tpope/vim-pathogen/raw/master/autoload/pathogen.vim -O ~/.vim/autoload/pathogen.vim
 bash -- "$here/DEPEND"
-[ -l ~/.vim/UltiSnips ] || ln -s -- "$here/snippets" ~/.vim/UltiSnips
-[ -l ~/.vim/usnippets ] || ln -s -- "$here/snippets" ~/.vim/usnippets
-[ -l ~/.vim/genproject ] || ln -s -- "$here/genproject" ~/.vim/genproject
-[ -l ~/.vim/scripts ] || ln -s -- "$here/scripts" ~/.vim/scripts
+[ -L ~/.vim/UltiSnips ] || ln -s -- "$here/snippets" ~/.vim/UltiSnips
+[ -L ~/.vim/usnippets ] || ln -s -- "$here/snippets" ~/.vim/usnippets
+[ -L ~/.vim/genproject ] || ln -s -- "$here/genproject" ~/.vim/genproject
+[ -L ~/.vim/scripts ] || ln -s -- "$here/scripts" ~/.vim/scripts
 for f in "$here/syntax"/*.vim; do
-	[ -l ~/.vim/syntax/"$(basename -- "$f")" ] || ln -s -- "$f" ~/.vim/syntax
+	[ -L ~/.vim/syntax/"$(basename -- "$f")" ] || ln -s -- "$f" ~/.vim/syntax
 done
 for f in "$here/nerdtree_plugin"/*.vim; do
-	[ -l ~/.vim/bundle/nerdtree/nerdtree_plugin/"$(basename -- "$f")" ] || ln -s -- "$f" ~/.vim/bundle/nerdtree/nerdtree_plugin
+	[ -L ~/.vim/bundle/nerdtree/nerdtree_plugin/"$(basename -- "$f")" ] || ln -s -- "$f" ~/.vim/bundle/nerdtree/nerdtree_plugin
 done
-[ -l ~/.vim/sniper-vimrc.vim ] || ln -s -- "$here/vimrc" ~/.vim/sniper-vimrc.vim
+[ -L ~/.vim/sniper-vimrc.vim ] || ln -s -- "$here/vimrc" ~/.vim/sniper-vimrc.vim
 touch ~/.vim/site-projects.vim
 if [ "$(test -f ~/.vimrc && head -1 ~/.vimrc)" != "runtime sniper-vimrc.vim" ]; then
 cat >>~/.vimrc <<EOF
